75cad7bd4b3cf77115cae872ecd2b23b4478c0b6
yuzhuzhai <zfq1109@gmail.com>
2021-09-03 21:48:39 -0400

    add table

--- sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
+
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("fuqiang", "zhai", "zfq@gmail.com", 100),
+    new SalesPerson("fuqiang2", "zhai2", "zfq2@gmail.com", 200),
+    new SalesPerson("fuqiang3", "zhai3", "zfq3@gmail.com", 300),
+    new SalesPerson("fuqiang4", "zhai4", "zfq4@gmail.com", 400)
+  
+  ]
+
 
   constructor() { }
 
